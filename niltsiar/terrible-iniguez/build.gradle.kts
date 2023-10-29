c3d1e1451d362a83cabb057b585c6c586ec63a46
Borja Quevedo <borjota@gmail.com>
2023-10-29 13:24:54 +0100

    Print results when run from command line

--- niltsiar/terrible-iniguez/build.gradle.kts
+++ niltsiar/terrible-iniguez/build.gradle.kts
@@ -1,4 +1,6 @@
 import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     application
@@ -38,6 +40,61 @@ tasks {
 
     withType<Test>().configureEach {
         useJUnitPlatform()
+        outputs.upToDateWhen { false }
+        testLogging {
+            lifecycle {
+                events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+                exceptionFormat = TestExceptionFormat.FULL
+
+                showExceptions = true
+                showCauses = true
+                showStackTraces = false
+                showStandardStreams = false
+            }
+            info.events = lifecycle.events
+            info.exceptionFormat = lifecycle.exceptionFormat
+        }
+
+        val failedTests = mutableListOf<TestDescriptor>()
+        val skippedTests = mutableListOf<TestDescriptor>()
+
+        addTestListener(object : TestListener {
+            override fun beforeSuite(suite: TestDescriptor) {}
+
+            override fun beforeTest(testDescriptor: TestDescriptor) {}
+
+            override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {
+                when (result.resultType) {
+                    TestResult.ResultType.FAILURE -> failedTests.add(testDescriptor)
+                    TestResult.ResultType.SKIPPED -> skippedTests.add(testDescriptor)
+                    else -> Unit
+                }
+            }
+
+            override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+                if (suite.parent == null) {
+                    logger.lifecycle("################ Summary::Start ################")
+                    logger.lifecycle("Test result: ${result.resultType}")
+                    logger.lifecycle(
+                        "Test summary: ${result.testCount} tests, " +
+                                "${result.successfulTestCount} succeeded, " +
+                                "${result.failedTestCount} failed, " +
+                                "${result.skippedTestCount} skipped"
+                    )
+                    failedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tFailed Tests")
+                    skippedTests.takeIf { it.isNotEmpty() }?.prefixedSummary("\tSkipped Tests:")
+                    logger.lifecycle("################ Summary::End ##################")
+                }
+            }
+
+            private infix fun List<TestDescriptor>.prefixedSummary(subject: String) {
+                logger.lifecycle(subject)
+                forEach { test -> logger.lifecycle("\t\t${test.displayName()}") }
+            }
+
+            private fun TestDescriptor.displayName() = parent?.let { "${it.name} - $name" } ?: "$name"
+
+        })
     }
 }
 
